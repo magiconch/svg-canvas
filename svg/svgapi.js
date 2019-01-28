e80312c973c977dada7dd84100c39189c801c138
magiconch <magiconch@outlook.com>
2019-01-28 15:15:05 +0800

    add drawDashedLineSvg

--- svg/svgapi.js
+++ svg/svgapi.js
@@ -4,10 +4,10 @@ function initDrawCanvas(canvas, ctx) { // 不太理解这个初始化的作用
   this.ctx = ctx
 }
 
-let svg = document.getElementById('');
-let canvas = document.getElementById('canvasTest');
-let ctx = canvas.getContext("2d");
-// svg.getAttributeNS
+let svg = document.getElementById('svgtest');
+// let canvas = document.getElementById('canvasTest');
+// let ctx = canvas.getContext("2d");
+
 
 // 绘制实线线段 
 // @param {Number} startX - 线段起点x轴坐标 
@@ -38,7 +38,7 @@ function drawSolidLineCanvas(startX, startY, endX, endY, lineWidth, color) {}
 // @param {Array} setLineDash - 点划线间距
 // @param {String} color - 线颜色
 function drawDashedLineSvg(startX, startY, endX, endY, setLineDash, lineWidth, color) {
-  let SolidLine = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+  let SolidLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
   SolidLine.setAttribute('x1', startX)
   SolidLine.setAttribute('y1', endX)
   SolidLine.setAttribute('x2', startY)
@@ -64,33 +64,32 @@ function drawDottedLine(startX, startY, endX, endY, radius, interval, color) {
   if (!interval) {
     interval = 5;
   }
-  var isHorizontal = true
-  if (startX == endX) {
-    isHorizontal = false;
-  }
-  var len = isHorizontal ? endX - startX : endY - startY
-  ctx.strokeStyle = color;
-  ctx.fillStyle = color;
-  ctx.save();
-  ctx.beginPath();
-  ctx.moveTo(startX, startY);
-  var progress = 0;
-  while (len > progress) {
-    progress += interval;
-    if (progress > len) {
-      progress = len;
-    }
-    if (isHorizontal) {
-      ctx.moveTo(startX + progress, startY);
-      ctx.arc(startX + progress, startY, radius, 0, Math.PI * 2, true);
-      ctx.fill();
-    } else {
-      ctx.moveTo(startX, endX + progress);
-      ctx.arc(startX, startY + progress, radius, 0, Math.PI * 2, true);
-      ctx.fill();
-    }
-  }
-  ctx.restore();
+  let dottedLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
 }
-drawDottedLine(10,10,200,200,2,10,'black');
-function drawRect(x, y, width, height, isClear) {}
+// drawDottedLine(10,10,200,200,2,10,'black');
+
+// 封装直角矩形
+// 矩形包括： 填充矩形、边框矩形和清除矩形区域 
+// @param {Number} x - 矩形起点的x坐标 
+// @param {Number} y - 矩形起点的y坐标 
+// @param {Number} width - 矩形宽度 
+// @param {Number} height - 矩形高度 
+// @param {Boolean} isFill - 是否填充；true绘制填充矩形， false绘制边框矩形 
+// @param {String} color - 矩形颜色
+function drawRect(x, y, width, height, isFill, color) {
+  let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+  rect.setAttribute('x',x);
+  rect.setAttribute('y',y);
+  rect.setAttribute('width',width);
+  rect.setAttribute('height',height);
+  if (isFill) {
+    rect.setAttribute('fill',color);
+    rect.setAttribute('stroke',color);
+  } else {
+    rect.setAttribute('fill-opacity',0);
+    rect.setAttribute('stroke',color);
+  }
+  svg.appendChild(rect);
+}
+// drawRect(10, 10, 100, 50, true, 'red');
+// drawRect(200, 200 , 50, 50, false, 'green');
